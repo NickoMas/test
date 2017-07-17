8f97c42c1f8813a417721866a4f2bc1eeb0c3daf
NickoMas <aquinnm@gmail.com>
2017-07-17 09:27:45 +0300

    feature: handle errors

--- task_promise/script.js
+++ task_promise/script.js
@@ -1,19 +1,19 @@
 
 fetch("https://server-for-promises.herokuapp.com/temp/story.json")
-    .then(response => response.json())
-    .then(response => {
+    .then(response => response.json()) // fetch object with header and links on chapters
+    .then(response => {                 //fetch all links asynchronously, append header into doc
         let header = document.createElement("h1");
         header.innerHTML = response.header;
         document.body.insertBefore(header, document.querySelector(".loading"))
         return Promise.all(Object.values(response.chapters)
                             .map(url => fetch(url)
-                                .then(item => item.json())))
+                                .then(item => item.json()))) //parse into objects with headers and text
     })
-    .then(item => {
-        item.sort((a,b) => {
+    .then(item => {                     //sort objects according to headers numbers so that to
+        item.sort((a,b) => {            //add them into doc in consistent way
             return +a.chapterNumber[8] - +b.chapterNumber[8]
         });
-        item.forEach((item) => {
+        item.forEach((item) => {        //add chapters with headers into doc
             let pHeader = document.createElement('h4');
             pHeader.innerHTML = item.chapterNumber;
             document.body.appendChild(pHeader)
@@ -22,4 +22,9 @@ fetch("https://server-for-promises.herokuapp.com/temp/story.json")
             document.body.appendChild(par);
         })
     })
-    .then(item => document.querySelector(".loading").style.display = "none")
+    .then(item => document.querySelector(".loading").style.display = "none") //switch off spinner
+    .catch(error => {
+        document.querySelector(".error").style.display = "block";   //handle errors for user
+        document.querySelector(".errorcode").innerHTML = `Your error is: ${error}`;
+        document.querySelector(".loading").style.display = "none"
+    })
