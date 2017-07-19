6ce0a856d88250751279864e1a7f0b8ff20e3b68
NickoMas <aquinnm@gmail.com>
2017-07-19 14:23:10 +0300

    feature: more of angular implementation

--- task_quiz/js/root.js
+++ task_quiz/js/root.js
@@ -1,22 +1,36 @@
 angular.module("root", [])
 	.controller("main", ["$scope", "$http", ($scope, $http) => {
 		
-		$scope.overall = 0;
+		$scope.overall = 0;	
+		$scope.correct = 0;
+		$scope.correctPercent = 0;
+
+		$scope.questions = null;
+
+		// color depends on percent of correct answers
+		$scope.markColor = () => (
+			{
+				'background-color': `hsl(${($scope.correctPercent / 100) *120},100%,50%)`
+			}
+		);
+
 
 		$scope.request = function () {
-			// var r = $resource("http://127.0.0.1:8080/quiz.json");
-			// r.get();
-			// console.log(r)
+
 			$http({
 				method: "GET",
 				url: "http://127.0.0.1:8080/quiz.json"
 			}).then(a => {
-				console.log($scope.overall)
+				$scope.correct = a.data.questions.filter(item => item.correct).length;
+
+				$scope.questions = a.data.questions;
+
 				$scope.overall = a.data.questions.length;
-				$scope.apply()
+
+				$scope.correctPercent = Math.round(($scope.correct / $scope.overall) * 100);
+				console.log($scope.markColor(), $scope.questions)
 			})
 
-			//$scope.overall = 
 		}
 
 
