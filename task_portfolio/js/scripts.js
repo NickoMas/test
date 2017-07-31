66b154ccd83dc2f41feab590655078310d973696
NickoMas <aquinnm@gmail.com>
2017-07-31 16:04:33 +0300

    portfolio/ scrolling

--- task_portfolio/js/scripts.js
+++ task_portfolio/js/scripts.js
@@ -1,6 +1,7 @@
 let currentOffset = 0;
 let panel = document.querySelector(".panel");
 let step = parseInt(getComputedStyle(panel).minHeight);
+let genHeight = parseInt(getComputedStyle(document.querySelector(".wrapper")).height);
 
 // const scrollToPlace = (dir) => {
 //     const go = setInterval(() => {
@@ -8,58 +9,31 @@ let step = parseInt(getComputedStyle(panel).minHeight);
 //     }, 10)
 // }
 
-// let promise = new Promise(function(resolve, reject) {
-//
-//     const go = setInterval(() => {
-// 	    document.body.scrollTop += 5;
-//     }, 10)
-//
-//     if(document.body.scrollTop === 200) {
-//         clearInterval(go);
-//         resolve();
-//     }
-// });
-
 const func = function (dir) {
     document.body.style.overflow = "hidden";
 
     if (dir === "down") {
         const fo = setInterval(() => {
             document.body.scrollTop += 1;
-            if (document.body.scrollTop === (currentOffset + step)) {
-                clearInterval(fo);
-                document.body.style.overflow = "visible";
+            if (document.body.scrollTop >= (currentOffset + step)) {
+                clearInterval(fo);console.log("fire!");
+                currentOffset = document.body.scrollTop;
+                // document.body.style.overflow = "visible";
             }
-        }, 10)
-    } else {
+        }, 5)
+    } else if (document.body.scrollTop) {
         const fo = setInterval(() => {
             document.body.scrollTop -= 1;
-            if (document.body.scrollTop === (currentOffset - step)) {
+            if (document.body.scrollTop <= (currentOffset - step)) {
                 clearInterval(fo);
-                document.body.style.overflow = "visible";
+                currentOffset = document.body.scrollTop;
+                // document.body.style.overflow = "visible";
             }
-        }, 10)
+        }, 5)
     }
-
-    // const fo = setInterval(() => {
-    //     if (dir === "down") {
-    //         document.body.scrollTop += 1;
-    //         if (document.body.scrollTop === (currentOffset + step)) {
-    //             clearInterval(fo);
-    //             document.body.style.overflow = "visible";
-    //         }
-    //     } else {
-    //         document.body.scrollTop -= 1;
-    //         if (document.body.scrollTop === (currentOffset - step)) {
-    //             clearInterval(fo)
-    //             document.body.style.overflow = "visible";
-    //         }
-    //     }
-    //
-    //     currentOffset = document.body.scrollTop;
-    // }, 10)
-    //clearInterval(fo)
 }
+
+//// setIntervals through promises >
 
 //
 // const com = function () {
@@ -87,7 +61,46 @@ const func = function (dir) {
 //     return promise
 // }
 
+//moving all main blocks simultaneously along Y-axis
 
-// window.addEventListener("scroll", () => {
-//     window.pageYOffset > currentOffset ? func("down") : func("up")
+
+//currentOffset = 0
+
+const move = function (dir, leap) {
+        if (dir === "down") {
+            if (-currentOffset + (leap || step) < genHeight){
+                currentOffset -= (leap || step);
+            }
+        } else if (-currentOffset - step >= 0) {
+            currentOffset += step;
+        }
+        document.querySelectorAll(".panel").forEach((a) => {
+            a.style.transform = `translateY(${currentOffset}px)`
+        });
+        //let anchor = window.location.href.match(/[$#](\w+)/)[0];
+        //location.assign(window.location.href.slice(0, -anchor.length))
+}
+
+window.addEventListener("mousewheel", (e) => {
+    e.preventDefault();
+
+    if(e.deltaY >= 0) {
+        setTimeout(() => {
+            move("down")
+        }, 200)
+    } else {
+        setTimeout(() => {
+            move("up")
+        }, 200)
+    }
+})
+
+// window.addEventListener("scroll", (e) => {
+//     if(window.pageYOffset > currentOffset) {
+//         move("down")
+//         console.log("down")
+//     } else if (window.pageYOffset < currentOffset) {
+//         move("up")
+//         console.log("up")
+//     }
 // })
