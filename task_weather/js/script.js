075d6ff3c5e1c476bd67a56d2acf34cc12e911a3
NickoMas <aquinnm@gmail.com>
2017-08-15 16:00:10 +0300

    weather/basic connection

--- task_weather/js/script.js
+++ task_weather/js/script.js
@@ -1,18 +1,31 @@
 "use strict"
 
+const myApiKEY = "AIzaSyCvU7DOgrnK1i4IkR9gFE9j7c4qyscb_FE"; // for google maps api
+
 angular.module("factory", [])
 	.factory("sendHttp", ["$http", 
 		function ($http) {
-			return $http({
-				method: "GET",
-				url : "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=95fce1ea5c2190183b85f3e518de44cd"
-			}).then(a => {
-				console.log(a.data)
-			})
+			return function sendHttp (arg) {
+				const cityName = arg.target.children[1].value;
+
+				console.log();
+				return $http({
+						method: "GET",
+						url : `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Vict&types=(cities)&language=pt_BR&key=AIzaSyCvU7DOgrnK1i4IkR9gFE9j7c4qyscb_FE`,
+						headers: {
+							"Access-Control-Allow-Origin": "*",
+							"Access-Control-Allow-Headers": "X-Requested-With",
+							"Access-Control-Allow-Credentials": "true"
+						}
+					}).then(a => {
+						console.log(a.data)
+					})	
+			}
 	}])
 
 angular.module("root", ["factory"])
 	.controller("main-Ctrl", ["$scope", "sendHttp", 
 		function ($scope, sendHttp) {
+			$scope.text = "";
 			$scope.send = sendHttp;
 	}])
